9bc2b84a13075decf0418afe857df5b07c2b0b42
kyuhoon <prelasia@gmail.com>
2016-12-15 17:41:42 +0900

    crypto 모듈 - 암호화, 복호화 예제

--- crypto.js
+++ crypto.js
@@ -1,11 +1,24 @@
 /**
  * Created by koh on 2016-12-15.
  */
-
 var crypto = require('crypto');
 
-var shasum = crypto.createHash('sha256');
-shasum.update('crypto_hash');
-var output = shasum.digest('hex');
+var key = '아무도 알지 못하는 나만의 비밀 키';
+var input = 'PASSWORD';
 
-console.log('crypto_hash:', output);
+//암호화
+var cipher = crypto.createCipher('aes192',key);
+cipher.update(input, 'utf8', 'base64');
+var cipheredOutput = cipher.final('base64');
+
+//암호화 해제
+var decipher = crypto.createDecipher('aes192', key);
+decipher.update(cipheredOutput, 'base64', 'utf8');
+var decipheredOutput = decipher.final('utf8');
+
+//출력
+console.log('원래 문자열: '+ input);
+console.log('암호화: '+ cipheredOutput);
+console.log('복호화: '+ decipheredOutput);
+
+
