d455c89d3385646cb893cd7712fab8beb74fec60
kyuhoon <prelasia@gmail.com>
2016-12-15 16:24:30 +0900

    url 모듈, querystring 모듈 사용 예제

--- url.js
+++ url.js
@@ -3,7 +3,7 @@
  */
 
 var url = require('url');
-
+var querystring = require('querystring');
 var parsedObject = url.parse('http://www.hanbit.co.kr/store/books/look.php?p_code=B3759307730');
 
-console.log(parsedObject);
+console.log(querystring.parse(parsedObject.query));
