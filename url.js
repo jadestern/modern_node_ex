55beb29a793456eb72d0be37c53c47a87f59e01d
kyuhoon <prelasia@gmail.com>
2016-12-15 16:27:49 +0900

    url 모듈에서 query 객체로 불러올 때의 예제

--- url.js
+++ url.js
@@ -6,4 +6,4 @@ var url = require('url');
 var querystring = require('querystring');
 var parsedObject = url.parse('http://www.hanbit.co.kr/store/books/look.php?p_code=B3759307730');
 
-console.log(querystring.parse(parsedObject.query));
+console.log(url.parse('http://www.hanbit.co.kr/store/books/look.php?p_code=B3759307730',true));
