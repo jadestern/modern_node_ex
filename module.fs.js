21b7463b9700587427f236bc3e71a55a91f5079d
kyuhoon <prelasia@gmail.com>
2016-12-16 10:54:00 +0900

    fs 모듈 예제 - 비동기 처리를 하는 메서드의 예외 처리

--- module.fs.js
+++ module.fs.js
@@ -5,19 +5,20 @@
 var fs = require('fs');
 
 //파일 읽기
-try {
-    var data = fs.readFileSync('textfile.txt', 'utf8');
-    console.log(data);
-} catch (e) {
-    console.log(e);
-}
+fs.readFile('textfile.txt', 'utf8', function (error, data) {
+    if( error ){
+        console.log(error);
+    } else {
+       console.log(data);
+    }
+});
 
 //파일 쓰기
-try {
-    fs.writeFileSync('textfile.txt', 'Hello World', 'utf8');
-    console.log('FILE WRITE COMPLETE');
-} catch (e) {
-    console.log(e);
-}
-
+fs.writeFile('textfile.txt', 'Hello World', 'utf8', function ( error ) {
+    if( error ){
+       console.log( error );
+    } else {
+       console.log( 'File WRITE COMPLETE' );
+    }
+});
 
