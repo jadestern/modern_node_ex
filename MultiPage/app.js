745ff1042cf748d64ce147033108f66eb867fa0b
kyuhoon <prelasia@gmail.com>
2016-12-16 16:44:24 +0900

    request 객체 이용 쿠키Cookie 저장 및 출력

--- MultiPage/app.js
+++ MultiPage/app.js
@@ -8,17 +8,19 @@ var url = require( 'url' );
 
 // 서버를 생성 및 실행
 http.createServer(function (request, response) {
-    if( request.method == 'GET' ){
-        fs.readFile('index.html', function (error, data) {
-            response.writeHead(200, {'Content-Type':'text/html'});
-            response.end(data);
-        })
-    } else if( request.method == 'POST' ){
-        request.on('data', function (data) {
-            response.writeHead(200, {'Content-Type':'text/html'});
-            response.end('<h1>'+ data +'</h1>');
-        });
-    }
+    var cookie = request.headers.cookie;
+
+    //SET Cookie
+    response.writeHead(200,{
+        'Content-Type':'text/html',
+        'Set-Cookie': [
+            'name = prelasia',
+            'region = Seoul'
+        ]
+    });
+
+    //응답
+    response.end('<h1>' + JSON.stringify(cookie) + '</h1>')
 
 }).listen(52273, function () {
     console.log( 'Server Running at http://localhost:52273' );
