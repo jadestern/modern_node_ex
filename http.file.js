b27f6be5ef9db47cfd4cf1f63f24b05c45f8114c
kyuhoon <prelasia@gmail.com>
2016-12-16 16:02:57 +0900

    redirect 예제(304)

--- http.file.js
+++ http.file.js
@@ -7,19 +7,11 @@ var http = require('http');
 
 //웹서버 생성 및 실행
 http.createServer(function (req, res) {
-    // 변수 선언
-    var date = new Date();
-    date.setDate(date.getDate() + 7);
 
-    // 쿠키를 입력
-    res.writeHead(200, {
-        'Content-Type': 'text/html',
-        'Set-Cookie': [
-            'breakfast = toast;Expires = ' + date.toUTCString(),
-            'dinner = chicken'
-        ]
+    res.writeHead(302, {
+        'Location': 'http://www.hanbit.co.kr'
     });
-    res.end('<h1>' + req.headers.cookie + '</h1>');
+    res.end();
 }).listen( 52273, function () {
     console.log( 'Server Running at http://localhost:52273' );
 });
