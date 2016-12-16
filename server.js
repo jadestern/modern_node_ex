cc1746200077e132e25a6f94777e29f565b39a70
kyuhoon <prelasia@gmail.com>
2016-12-16 12:51:31 +0900

    server - 이벤트 연결

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 //서버 생성
 var server = require('http').createServer();
 
-//웹 서버 실행
-server.listen(52273, function () {
-    console.log( 'Server Runiing at http://localhost:52273' );
+//server 객체에 이벤트 연결
+server.on('request', function (code) {
+    console.log( 'Request On' );
 });
 
-//10초 후에 함수를 실행
-var test = function () {
-    //서버를 종료
-    server.close();
-};
-setTimeout(test, 5000);
+server.on('connection', function (code) {
+    console.log( 'Connection On' );
+});
+
+server.on('close', function (code) {
+    console.log( 'Close On' );
+});
+
+server.listen(52273);
