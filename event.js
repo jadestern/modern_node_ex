f6b88df02f69f4496aa35672e936ddb35e7c0411
kyuhoon <prelasia@gmail.com>
2016-12-16 12:27:01 +0900

    event 관련 예제 - 이벤트 생성

--- event.js
+++ event.js
@@ -2,15 +2,11 @@
  * Created by koh on 2016-12-16.
  */
 
-//process 객체에 EXIT 이벤트를 연결
-process.on('exit', function (code) {
-    console.log( '안녕히 가세욤' );
+//EventEmitter 객체를 생성
+var custom = new process.EventEmitter();
+
+custom.on('tick', function (code) {
+    console.log( '이벤트를 실행' );
 });
 
-
-process.emit('exit');
-process.emit('exit');
-process.emit('exit');
-process.emit('exit');
-
-console.log( '프로그램 실행중' );
+custom.emit('tick');
