f2a2e9ebb8c5ab22bc5ac6055afd5d794316df59
kyuhoon <prelasia@gmail.com>
2016-12-16 18:05:53 +0900

    Jade 사용 예제

---
+++ jade/jade.basic.js
@@ -0,0 +1,19 @@
+/**
+ * Created by koh on 2016-12-16.
+ */
+
+var http = require( 'http' );
+var fs = require( 'fs' );
+var jade = require( 'jade' );
+
+http.createServer(function (req, res) {
+    fs.readFile('JadePage.jade', 'utf8', function (error, data) {
+        var fn = jade.compile(data);
+
+        //출력
+        res.writeHead(200, {'Content-Type':'text/html'});
+        res.end(fn());
+    });
+}).listen(52273, function () {
+    console.log( 'Server Running at localhost:52273' );
+});
