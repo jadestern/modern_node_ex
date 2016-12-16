2b55985a0d4b6ff8d36692e89f3f32117c29d303
kyuhoon <prelasia@gmail.com>
2016-12-16 11:59:13 +0900

    fs 모듈 예제 - 예외 조기 리턴

--- module.fs.js
+++ module.fs.js
@@ -6,19 +6,13 @@ var fs = require('fs');
 
 //파일 읽기
 fs.readFile('textfile.txt', 'utf8', function (error, data) {
-    if( error ){
-        console.log(error);
-    } else {
-       console.log(data);
-    }
+    if ( error ) { return console.error(error) }
+    console.log( data );
 });
 
 //파일 쓰기
 fs.writeFile('textfile.txt', 'Hello World', 'utf8', function ( error ) {
-    if( error ){
-       console.log( error );
-    } else {
-       console.log( 'File WRITE COMPLETE' );
-    }
+    if ( error ) { return console.error(error) }
+    console.log( 'FILE WRITE COMPLETE' );
 });
 
