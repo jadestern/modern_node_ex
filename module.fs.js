67401fb7abe39de45c3ef45ee9e700f1f67fd136
kyuhoon <prelasia@gmail.com>
2016-12-16 10:34:04 +0900

    fs 모듈 예제 - 비동기식

--- module.fs.js
+++ module.fs.js
@@ -4,5 +4,6 @@
 
 var fs = require('fs');
 
-var text = fs.readFileSync('textfile.txt', 'utf8');
-console.log(text);
+fs.readFile('textfile.txt', 'utf8', function (error, data) {
+   console.log(data);
+});
