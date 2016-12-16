c466215829bfb4c4053f882fbb71a414860333d3
kyuhoon <prelasia@gmail.com>
2016-12-16 16:24:44 +0900

    GET 요청 매개변수 추출(JSON stringfy)

--- MultiPage/app.js
+++ MultiPage/app.js
@@ -8,14 +8,12 @@ var url = require( 'url' );
 
 // 서버를 생성 및 실행
 http.createServer(function (request, response) {
-    var pathname = url.parse(request.url).pathname;
+    //요청 매개변수를 추출
+    var query = url.parse(request.url, true).query;
 
-    //페이지를 구분
-    if( request.method == 'GET' ){
-        console.log('GET 요청')
-    } else if( request.method == 'POST' ){
-        console.log( 'POST요청' );
-    }
+    //GET 요청 매개변수 출력
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end('<h1>'+ JSON.stringify(query) +'</h1>')
 
 }).listen(52273, function () {
     console.log( 'Server Running at http://localhost:52273' );
