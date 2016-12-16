293384d52126290727b804749e9dc794e4642cb2
kyuhoon <prelasia@gmail.com>
2016-12-16 10:38:14 +0900

    fs 모듈 예제 - 동기식, 비동기식 쓰기(write)

--- module.fs.js
+++ module.fs.js
@@ -7,3 +7,13 @@ var fs = require('fs');
 fs.readFile('textfile.txt', 'utf8', function (error, data) {
    console.log(data);
 });
+
+var data = 'Hello World!';
+
+//모듈 사용
+fs.writeFile('TextFileOtherWrite.txt', data, 'utf8', function (err) {
+   console.log('WRITE FILE ASYNC COMPLETE');
+});
+
+fs.writeFileSync('TextFileOtherWriteSync.txt', data, 'utf8');
+console.log('WRITE FILE SYNC COMPLETE');
