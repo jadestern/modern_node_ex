d8b65bad2a283ce686af55abd9c8eec265e3cca4
kyuhoon <prelasia@gmail.com>
2016-12-16 12:21:17 +0900

    event 관련 예제 - 이벤트 제거.

--- event.js
+++ event.js
@@ -7,18 +7,18 @@ process.on('exit', function (code) {
     console.log( '안녕히 가세욤' );
 });
 
-process.on('uncaughtException', function (error) {
-   console.log( '예외 발생!' );
-});
+var onUncaughtException = function (error) {
+    console.log( '예외 발생!' );
 
-var count = 0;
+    process.removeListener('uncaughtException', onUncaughtException);
+};
+
+process.on('uncaughtException', onUncaughtException);
+
+//2초 간격으로 예외 발생
 var test = function () {
-    //탈출 코드
-    count = count + 1;
-    if( count > 3 ) {return;}
-
-    //예외를 강제로 발생
     setTimeout(test, 2000);
     error.error.error();
 };
+
 setTimeout( test, 2000);
