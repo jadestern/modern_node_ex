dcdaea55ccb6aba9513fa6697767ef579e610ff7
kyuhoon <prelasia@gmail.com>
2016-12-16 16:33:19 +0900

    POST 요청 매개변수 추출

--- MultiPage/app.js
+++ MultiPage/app.js
@@ -8,12 +8,17 @@ var url = require( 'url' );
 
 // 서버를 생성 및 실행
 http.createServer(function (request, response) {
-    //요청 매개변수를 추출
-    var query = url.parse(request.url, true).query;
-
-    //GET 요청 매개변수 출력
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.end('<h1>'+ JSON.stringify(query) +'</h1>')
+    if( request.method == 'GET' ){
+        fs.readFile('index.html', function (error, data) {
+            response.writeHead(200, {'Content-Type':'text/html'});
+            response.end(data);
+        })
+    } else if( request.method == 'POST' ){
+        request.on('data', function (data) {
+            response.writeHead(200, {'Content-Type':'text/html'});
+            response.end('<h1>'+ data +'</h1>');
+        });
+    }
 
 }).listen(52273, function () {
     console.log( 'Server Running at http://localhost:52273' );
