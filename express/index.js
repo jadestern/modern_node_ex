562f7aae40fd87ec34a69ac33014e0e9f1f32744
kyuhoon <prelasia@gmail.com>
2016-12-19 15:25:04 +0900

    express - query 추출

--- express/index.js
+++ express/index.js
@@ -10,16 +10,12 @@ var app = express();
 
 //미들웨어 설정
 app.use(function (req, res) {
-    //User-Agent 속성 추출
-    var agent = req.header('User-Agent');
+    //변수 선언
+    var name = req.query.name;
+    var region = req.query.region;
 
-    //브라우저 구분
-    if( agent.toLowerCase().match(/chrome/) ){
-        //페이지 출력
-        res.send('<h1>Hello Chrome!</h1>')
-    } else {
-        res.send('<h1>Hello express!</h1>')
-    }
+    //응답합니다.
+    res.send('<h1>' + name + ' - ' + region + '</h1>')
 });
 
 app.listen(52273, function () {
