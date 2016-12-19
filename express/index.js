4c43575f8e16d091238310057104266f75afb5cc
kyuhoon <prelasia@gmail.com>
2016-12-19 12:11:10 +0900

    express - JSON send 예제

--- express/index.js
+++ express/index.js
@@ -3,12 +3,26 @@
  */
 
 //모듈 추출
-var http = require( 'http' );
 var express = require( 'express' );
 
 //서버 생성
 var app = express();
 
-http.createServer(app).listen(52273, function () {
-    console.log( "Server Running at http://127.0.0.1:52273" );
+//request 이벤트 리스너 설정
+app.use(function (req, res) {
+    //데이터 생성
+    var output = [];
+    for( var i = 0 ; i < 3 ; i++ ){
+        output.push({
+            count:i,
+            name:'name - ' + i
+        });
+    }
+
+    //응답
+    res.send(output);
+});
+
+app.listen(52273, function () {
+   console.log( "Server running at http://localhost:52273" );
 });
