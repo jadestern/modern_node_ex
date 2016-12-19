9c96e52dd52f1ccc1a213b3abd8178624e3c883b
kyuhoon <prelasia@gmail.com>
2016-12-19 14:58:16 +0900

    express - status 예제

--- express/index.js
+++ express/index.js
@@ -10,17 +10,9 @@ var app = express();
 
 //request 이벤트 리스너 설정
 app.use(function (req, res) {
-    //데이터 생성
-    var output = [];
-    for( var i = 0 ; i < 3 ; i++ ){
-        output.push({
-            count:i,
-            name:'name - ' + i
-        });
-    }
 
     //응답
-    res.send(output);
+    res.status(404).send("<h1>ERROR</h1>");
 });
 
 app.listen(52273, function () {
