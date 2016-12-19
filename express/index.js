dc524f335c57bc562bcb8504f83180e76ba8d216
kyuhoon <prelasia@gmail.com>
2016-12-19 15:35:45 +0900

    express - middleware에 데이터 추가

--- express/index.js
+++ express/index.js
@@ -3,34 +3,24 @@
  */
 
 //모듈 추출
-var express = require( 'express' );
+var express = require('express');
 
 //서버 생성
 var app = express();
 
-//미들웨어 설정 - 1
+//미들웨어 설정
 app.use(function (req, res, next) {
-   console.log( 'first' );
-   next();
-});
-
-//미들웨어 설정 - 2
-app.use(function (req, res, next) {
-    console.log( 'second' );
+    //데이터 추가
+    req.number = 52;
+    res.number = 273;
     next();
 });
 
-//미들웨어 설정 - 3
 app.use(function (req, res, next) {
-    console.log( 'third' );
-
     //응답
-    res.writeHead(200, {'Content-type': 'text/html'});
-    res.end('<h1>express middleware</h1>');
+    res.send('<h1>'+ req.number + ' : ' + res.number  +'</h1>')
 });
 
-
-
 app.listen(52273, function () {
-   console.log( "Server running at http://localhost:52273" );
+    console.log("Server running at http://localhost:52273");
 });
