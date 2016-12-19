1a996cd326275100b90e6c4a8adc289964579308
kyuhoon <prelasia@gmail.com>
2016-12-19 17:11:15 +0900

    express - morgan(log 출력)

---
+++ morgan/index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by koh on 2016-12-19.
+ */
+
+var express = require( 'express' );
+var morgan = require( 'morgan' );
+
+var app = express();
+
+app.use(morgan('combined'));
+app.use(function (req, res) {
+    res.send('<h1>Express Basic</h1>');
+
+});
+
+app.listen(52273, function () {
+    console.log("Server running at http://localhost:52273");
+});
