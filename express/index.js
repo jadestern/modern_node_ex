47ade61d302829a9ae78d86e48f066d1b252a83a
kyuhoon <prelasia@gmail.com>
2016-12-19 15:33:10 +0900

    express - middleware 추가

--- express/index.js
+++ express/index.js
@@ -8,15 +8,28 @@ var express = require( 'express' );
 //서버 생성
 var app = express();
 
-//미들웨어 설정
-app.use(function (req, res) {
-    //변수 선언
-    var name = req.query.name;
-    var region = req.query.region;
-
-    //응답합니다.
-    res.send('<h1>' + name + ' - ' + region + '</h1>')
+//미들웨어 설정 - 1
+app.use(function (req, res, next) {
+   console.log( 'first' );
+   next();
 });
+
+//미들웨어 설정 - 2
+app.use(function (req, res, next) {
+    console.log( 'second' );
+    next();
+});
+
+//미들웨어 설정 - 3
+app.use(function (req, res, next) {
+    console.log( 'third' );
+
+    //응답
+    res.writeHead(200, {'Content-type': 'text/html'});
+    res.end('<h1>express middleware</h1>');
+});
+
+
 
 app.listen(52273, function () {
    console.log( "Server running at http://localhost:52273" );
