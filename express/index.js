de48954b50ea01089d666690ff04a1babb36d025
kyuhoon <prelasia@gmail.com>
2016-12-19 15:50:39 +0900

    express - 라우팅 + 매개변수 사용

--- express/index.js
+++ express/index.js
@@ -8,17 +8,12 @@ var express = require('express');
 //서버 생성
 var app = express();
 
-//미들웨어 설정
-app.use(function (req, res, next) {
-    //데이터 추가
-    req.number = 52;
-    res.number = 273;
-    next();
-});
+app.get('/page/:id', function (req, res) {
+    //변수 선언
+    var name = req.params.id;
 
-app.use(function (req, res, next) {
     //응답
-    res.send('<h1>'+ req.number + ' : ' + res.number  +'</h1>')
+    res.send('<h1>'+ name + ' Page</h1>')
 });
 
 app.listen(52273, function () {
